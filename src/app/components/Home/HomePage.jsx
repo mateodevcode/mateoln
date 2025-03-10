fffe3857ca93d5d8fb5017b70d63457e50bcaeca
Mateo lizcano <mateodevcode@gmail.com>
2025-03-10 14:51:46 +0100

    Se mejoro el diseño de la pagina de inicio y se eliminaron archivos innecesarios

--- src/app/components/Home/HomePage.jsx
+++ src/app/components/Home/HomePage.jsx
@@ -1,7 +1,10 @@
 "use client"
-
 import Image from "next/image";
-import { botones } from "@/data/enlaces";
+import { IoLogoLinkedin } from "react-icons/io5";
+import { FaGithub, FaWhatsapp } from "react-icons/fa";
+import { IoIosApps } from "react-icons/io";
+import { SiHackthebox, SiTryhackme } from "react-icons/si";
+import { GrCertificate } from "react-icons/gr";
 
 const HomePage = () => {
   return (
@@ -19,6 +22,7 @@ const HomePage = () => {
         </h2>
         <div className="flex flex-row justify-center items-center xl:text-lg lg:text-base md:text-xs smd:text-xs sm:text-[10px]">
           <p>Desarrollador de software</p>
+          {/* <MdStars className="xl:mx-4 lg:mx-2 md:mx-2 smd:mx-2 sm:mx-2 text-2xl text-blue-600" /> */}
           <Image src="/logo-mateo.png" alt="Logo Mateo Lizcano" width={100} height={100} className="xl:mx-4 lg:mx-2 md:mx-2 smd:mx-2 sm:mx-2 w-8 " />
           <p>Analista de ciberseguridad</p>
         </div>
@@ -29,13 +33,42 @@ const HomePage = () => {
           </p>
         </div>
         <div className="flex flex-col justify-start items-center">
-          {botones.map((boton) => (
-            <button key={boton.id} className={`font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2 ${boton.color}`}
-              onClick={() => window.open(boton.url, "_blank")}
-              >
-                {boton.icono} {boton.nombre}
-              </button>
-          ))}
+          <button className="bg-[#2867B2] hover:bg-[#5d97dd] text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://www.linkedin.com/in/mateo-lizcano-noriega/", "_blank")}
+          >
+            <IoLogoLinkedin className="mr-2 text-xl" /> LinkedIn
+          </button>
+          <button className="bg-black hover:bg-black/50 text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://github.com/mateodevcode", "_blank")}
+          >
+            <FaGithub className="mr-2 text-xl" /> GitHub
+          </button>
+          <button className="bg-green-600 hover:bg-green-500 text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://mateo-lizcano-noriega.vercel.app/", "_blank")}
+          >
+            <IoIosApps className="mr-2 text-xl" /> Proyectos
+          </button>
+          <button className="bg-red-700 hover:bg-red-600 text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://tryhackme.com/p/mateo.dev", "_blank")}
+          >
+            <SiTryhackme className="mr-2 text-xl" /> TryHackMe
+          </button>
+
+          <button className="bg-[#0B121F] hover:bg-[#172235] text-green-600 font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://app.hackthebox.com/profile/1472461", "_blank")}
+          >
+            <SiHackthebox className="mr-2 text-xl" /> Hack The Box
+          </button>
+          <button className="bg-yellow-400 hover:bg-yellow-300 text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://www.credly.com/users/mateo-lizcano-noriega/badges", "_blank")}
+          >
+            <GrCertificate className="mr-2 text-xl" /> Certificaciones
+          </button>
+          <button className="bg-green-500 hover:bg-green-400 text-white font-bold py-2 px-4 rounded w-72 text-lg flex flex-row justify-center items-center shadow-sm shadow-black/50 my-2"
+            onClick={() => window.open("https://wa.me/34675464502", "_blank")}
+          >
+            <FaWhatsapp className="mr-2 text-xl" /> WhatsApp
+          </button>
         </div>
       </main>
       <footer className="text-center mt-10">
